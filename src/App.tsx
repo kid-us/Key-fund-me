e4e30d48cdbab0e2d091bea36057d66ff35bb913
kid-us <kidushh29@gmail.com>
2024-10-05 19:40:45 +0300

    Development setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="container mx-auto">
+        <p className="text-xl font-bold font-mono">Welcome to key fund me.</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
