83e8a5db33adb9f3dd0835b68065246e38afd152
Kid-us <kidushh29@gmail.com>
2024-10-11 13:32:51 +0300

    Back button changed to button

--- src/components/Button/BackButton.tsx
+++ src/components/Button/BackButton.tsx
@@ -1,16 +1,18 @@
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 interface Props {
   link: string;
 }
 const BackButton = ({ link }: Props) => {
+  const navigate = useNavigate();
+
   return (
-    <Link
-      to={link}
+    <button
+      onClick={() => navigate(link)}
       className="bg-neutral-900 w-60 rounded-lg text-white py-3 shadow shadow-zinc-900 text-center"
     >
       Back
-    </Link>
+    </button>
   );
 };
 
