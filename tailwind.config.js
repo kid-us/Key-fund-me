fb455d76bb4bf2ef70a79b26f4c57444aba5241b
Kid-us <kidushh29@gmail.com>
2024-10-21 17:04:37 +0300

    New Logo and color's

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "primary-bg": "#00a651", // Wrap color values in quotes
+        "secondary-bg": "#00adee", // Wrap color values in quotes
+      },
+    },
   },
   plugins: [],
 };
